e900be74155a0546b9d2d77674692a4a364ebc83
Matt Fehskens <matt.fehskens@gmail.com>
2017-07-20 23:44:26 -0400

    #7 Tests for .checkIsForExamples

--- test/utilities.spec.js
+++ test/utilities.spec.js
@@ -140,3 +140,17 @@ tap.test('#testIsDashFormat', (suite) => {
 
     suite.end();
 });
+
+tap.test('#checkIsForExamples', (test) => {
+    const check = utilities.checkIsForExamples;
+
+    test.plan(5);
+
+    test.notOk(check({ apple: [], broccoli: [] }));
+    test.ok({ example: [] });
+    test.ok({ examples: [] });
+    test.ok({ x: [] });
+    test.ok(check({ apple: [], examples: [] }));
+
+    test.end();
+});
