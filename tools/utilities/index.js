602508a3c641934821abbe6738905c0d87f8460a
Matt Fehskens <Matt Fehskens>
2017-08-04 13:19:21 -0400

    #66 Moved the library version methods to the files utility

--- tools/utilities/index.js
+++ tools/utilities/index.js
@@ -13,37 +13,3 @@ exports.checkIsScopedName = (name) =>
     // followed by /
     // folloer by 1+ non-/
     /^@[^/]+[/][^/]+$/.test(name);
-
-exports.getLibrarianVersion = () => {
-    const files = exports.files;
-    const pkgVersion = getPackageLibrarianVersion();
-    let version;
-
-    // if they have specified a URL, keep the URL
-    if (/^(git+)?http\:\:/.test(pkgVersion)) {
-        version = pkgVersion;
-    } else {
-        // otherwise, use the version from the package
-        version = files.include(
-            files.resolver.manual(__dirname, '..', '..', 'package.json')
-        ).version;
-    }
-
-    return version;
-};
-
-const getPackageLibrarianVersion = () => {
-    const files = exports.files;
-    const pkg = files.include(
-        files.resolver.root('package.json')
-    );
-    let version;
-
-    if (pkg.devDependencies && 'angular-librarian' in pkg.devDependencies) {
-        version = pkg.devDependencies['angular-librarian'];
-    } else if (pkg.dependencies && 'angular-librarian' in pkg.dependencies) {
-        version = pkg.dependencies['angular-librarian'];
-    }
-
-    return version;
-};
